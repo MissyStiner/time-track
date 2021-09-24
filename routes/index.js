4507f20c613f9ea377716d67dfc79ae24fcbf4e7
Missy Stiner <missystiner@gmail.com>
2021-09-24 18:42:01 -0500

    npm start errors resolved

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 
+const { restore } = require('../models/User');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.status(404).end();
+    if (req.url === '/hi') {
+        return res.status(200).json({message: 'you said hi'})
+    }
+    res.status(200).json({
+        url: req.url
+    })
+//   res.status(404).end();
 });
 
 module.exports = router;
