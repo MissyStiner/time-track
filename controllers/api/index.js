7fdbd9e34a830dd0416d85085fb0b3d82283316a
Missy Stiner <missystiner@gmail.com>
2021-09-27 18:20:47 -0500

    handlebars and homepage connection established

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const homeRoutes = require('../home-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User } = require('../models');
+
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'time',
+        'day',
+        'created_at',
+        // [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        //console.log(dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
+
+// router.get('/', (req, res) => {
+//     res.render('homepage', {
+//       id: 1,
+//       time: '8.925 seconds',
+//       day: 'Monday',
+//       created_at: new Date(),
+//       //like_count: 10,
+//       user: {
+//         username: 'test_user'
+//       }
+//     });
+//   });
