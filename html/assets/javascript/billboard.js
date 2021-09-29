b6206b10b7346db127e90b18327c9560b1b63bd4
Missy Stiner <missystiner@gmail.com>
2021-09-29 15:15:34 -0500

    styling applied to handlebars round 1

---
+++ html/assets/javascript/billboard.js
@@ -0,0 +1,10 @@
+var bar chart = bb.generate({
+    bindto: "#chart",
+    data: {
+        type: "bar",
+        columns: [
+            ["data1", 30, 200, 100, 170, 150, 250],
+            ["data2", 130, 100, 140, 35, 110, 50]
+        ]
+    }
+});
