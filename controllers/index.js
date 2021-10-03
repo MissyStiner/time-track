ce2615cc6f2bee5296f7e5a04bf9dce70ee516b5
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-10-03 13:43:05 -0500

    removed duplicate lines from index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,6 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
