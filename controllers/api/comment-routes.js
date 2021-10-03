70781d607dd17ccf3dfc2805e9aad6ba3d219d34
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-10-03 16:20:28 -0500

    pull developer

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,23 +2,19 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// get all comments
 router.get('/', (req, res) => {
-  Comment.findAll()
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+  Comment.findAll({}).then(dbCommentData => res.json(dbCommentData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+})
 
-// create comment
 router.post('/', withAuth, (req, res) => {
-    // check session
   Comment.create({
-    comment_body: req.body.comment_body,
-    user_id: req.session.user_id,
-    post_id: req.body.post_id
+    comment_text: req.body.comment_text,
+    post_id: req.body.post_id,
+    user_id: req.session.user_id
   })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
@@ -27,24 +23,23 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-// delete comment
 router.delete('/:id', withAuth, (req, res) => {
   Comment.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbCommentData => {
-      if (!dbCommentData) {
-        res.status(404).json({ message: 'No comment found with this id!' });
-        return;
+      where: {
+        id: req.params.id
       }
-      res.json(dbCommentData);
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then(dbCommentData => {
+        if (!dbCommentData) {
+          res.status(404).json({ message: 'No comment found with this id' });
+          return;
+        }
+        res.json(dbCommentData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 module.exports = router;
