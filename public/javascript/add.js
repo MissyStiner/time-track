2567b04c8e87ba9aff99a354c4a1a53b8dd4a135
Missy Stiner <missystiner@gmail.com>
2021-10-03 19:04:28 -0500

    labels changed

--- public/javascript/add.js
+++ public/javascript/add.js
@@ -1,14 +1,14 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
+  const post_date = document.querySelector('input[name="post-date"]').value;
   const post_day = document.querySelector('input[name="post-day"]').value;
-  const post_hour = document.querySelector('input[name="post-hour"]').value;
+  const post_time = document.querySelector('input[name="post-time"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
-      title,
+      post_date,
       post_day,
       post_time
     }),
