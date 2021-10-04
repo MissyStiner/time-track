8a24cef40bf48d0e501b2bcbad028f151f7c6d00
Derimar Gray <derimargray@gmail.com>
2021-10-03 22:10:58 -0500

    update chart

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const sess = {
 app.use(session(sess));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
