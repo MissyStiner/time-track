5aad1c4949e3b973bcfcf6202ce989cf479d4692
Derimar Gray <derimargray@gmail.com>
2021-10-03 19:21:20 -0500

    update edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,7 +1,7 @@
 async function editFormHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="post-title"]').value;
+    const post_date = document.querySelector('input[name="post-date"]').value;
     const post_day = document.querySelector('input[name="post-day"]').value;
     const post_hour = document.querySelector('input[name="post-hour"]').value;
     const id = window.location.toString().split('/')[
@@ -11,7 +11,7 @@ async function editFormHandler(event) {
     const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            title,
+            post_date,
             post_day,
             post_hour
         }),
